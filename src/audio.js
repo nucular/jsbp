7eaca444674c19ba8c71924141044d7204e02a2c
nucular <nucular@users.noreply.github.com>
2014-08-05 00:45:55 +0200

    Fixed audio interpolation

--- src/audio.js
+++ src/audio.js
@@ -5,7 +5,6 @@ JSBP.audio.init = function() {
 
     // Create the buffer
     JSBP.audio.buffer = JSBP.audio.context.createBuffer(1, 256 * 2, 15360 * 2);
-    JSBP.audio.bufferF32 = new Float32Array(256);
 
     // This is were we'll write the processed audio block to
     JSBP.audio.data = JSBP.audio.buffer.getChannelData(0);
@@ -14,37 +13,27 @@ JSBP.audio.init = function() {
     JSBP.audio.node = JSBP.audio.context.createBufferSource(0);
     JSBP.audio.node.loop = true;
     JSBP.audio.node.buffer = JSBP.audio.buffer;
-    JSBP.audio.node.noteOn(JSBP.audio.context.currentTime);
+    JSBP.audio.node.start();
 }
 
 JSBP.audio.tick = function() {
-    // Fetch the audio block location
-    var start = JSBP.mem[6]<<16 | JSBP.mem[7]<<8;
-    var j;
-
-    // From signed 8-bit to signed float
-    for (var i = 0; i < 0xFF; i++) {
-        //JSBP.audio.data[i] = (j > 0) ? j / 127 : j / -127;
-        JSBP.audio.bufferF32[i] = (JSBP.mem[start | i] / 255.0) - 1;
+    var s8ToFloat = function(s) {
+        return (s >= 128 ? s - 256 : s) / 127;
     }
 
+    // Fetch the audio block location
+    var start = JSBP.mem[6] << 16 | JSBP.mem[7] << 8;
+
     // Interpolate from 15360 to 30720
-    var mu = 0.2, mu2 = (1 - Math.cos(mu * Math.PI)) / 2;
-    var point1, point2;
+    var s1, s2;
 
-    for (var i = 0; i < 128; i++) {
-        j = i * 2;
+    for (var i = 0; i <= 256; i++) {
+        // Get samples
+        s1 = s8ToFloat(JSBP.mem[start | i]);
+        s2 = (i >= 255) ? s1 : s8ToFloat(JSBP.mem[start | (i + 1)]);
 
-        // The points to interpolate between
-        point1 = JSBP.audio.bufferF32[i];
-        point2 = (i >= 255) ? point1 : JSBP.audio.bufferF32[i + 1];
-
-        // interpolate
-        point2 = (point1 * (1 - mu2) + point2 * mu2);
-        //point2 = (point1 + point2) / 2;
-
-        // write
-        JSBP.audio.data[j] = point1;
-        JSBP.audio.data[j + 1] = point2;
+        // Interpolate and write
+        JSBP.audio.data[i * 2] = s1;
+        JSBP.audio.data[i * 2 + 1] = (s1 + s2) / 2;
     }
 }
