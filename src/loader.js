9d2c465bddc04c7404c19ada8463c61c49a7ffb2
nucular <nucular@users.noreply.github.com>
2014-08-05 14:53:09 +0200

    Added some style

--- src/loader.js
+++ src/loader.js
@@ -1,8 +1,11 @@
 JSBP.loader = {};
 
 JSBP.loader.init = function() {
-    $("#screen").bind("dragover", JSBP.loader.dragOver);
-    $("#screen").bind("drop", JSBP.loader.drop);
+    $("#overlay").bind("dragenter", JSBP.loader.dragEnter);
+    $("#overlay").bind("drop", JSBP.loader.drop);
+
+    $(document).bind("dragover", JSBP.loader.docDragOver);
+    $(document).bind("drop", JSBP.loader.docDrop);
 }
 
 JSBP.loader.loadFile = function(file) {
@@ -27,7 +30,7 @@ JSBP.loader.loadFile = function(file) {
     reader.readAsArrayBuffer(file);
 }
 
-JSBP.loader.dragOver = function(e) {
+JSBP.loader.dragEnter = function(e) {
     e.stopPropagation();
     e.preventDefault();
     // Show that we won't move anything
@@ -48,4 +51,37 @@ JSBP.loader.drop = function(e) {
         JSBP.loader.loadFile(file);
     else
         alert("File too big");
+}
+
+
+JSBP.loader.docDragOver = (function() {
+    var interval, isover;
+
+    // Hacky workaround
+    return function(e) {
+        e.preventDefault();
+
+        clearInterval(interval);
+
+        interval = setInterval(function() {
+            isover = false;
+            clearInterval(interval);
+
+            $("#overlay").fadeOut("fast", function() {
+                $("#overlay").css("display", "none");
+            });
+        }, 100);
+
+        if (!isover) {
+            isover = true;
+            e.originalEvent.dataTransfer.dropEffect = "none";
+
+            $("#overlay").fadeTo("fast", 0.8);
+        }
+    }
+})();
+
+JSBP.loader.docDrop = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
 }
